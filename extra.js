0d38f34c6dd81f30c4d70fb7569699b8725f2674
unknown <sakethpippari@gmail.com>
2022-10-06 22:22:20 +0530

    tri

---
+++ extra.js
@@ -0,0 +1,15 @@
+/*let a=5
+let b=6
+let c=7
+let sum=(a+b+c)/2
+let sum1=sum*(sum-a)*(sum-b)*(sum-c)
+let sqr=sum1**(1/2)
+document.write("area of triangle is: "+sqr)*/
+/*var d=12
+var e=12
+var f=(d=e)? (d+e)*3:d+e
+document.write(f)*/
+filename="system.php"
+console.log(filename.split('.').pop());
+filename="abc.js"
+console.log(filename.split('.').pop());
